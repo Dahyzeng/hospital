16b39f68c8c174d7199dd5cb68a201669c3407c7
wendy.sun <wendy.sun@bytesforce.com>
2021-01-06 23:45:32 +0800

    psq

--- src/layout/router.ts
+++ src/layout/router.ts
@@ -7,7 +7,22 @@ export default {
   children: [
     {
       path: '/plan',
+      name: 'name',
+      meta: {
+        title: "用药计划"
+        // left/right 导航栏左右插槽
+        // left: Test,
+        // 不显示导航栏
+        // header: false,
+      },
       component: () => import("@/views/Plan/index.vue"),
+    },
+    {
+      path: '/psq',
+      meta: {
+        title: "配置用药反应症状问卷"
+      },
+      component: () => import("@/views/PSQ/index.vue"),
     }
   ]
 } as RouteConfig;
---
+++ src/views/PSQ/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="psq">
+    <div>
+      <header>
+        <img alt="Vue logo" src="../../assets/symptom.png" />
+      </header>
+      <van-checkbox-group v-model="result">
+        <van-cell-group>
+          <van-cell
+            v-for="(item, index) in symptonList"
+            clickable
+            :key="item"
+            :title="`${item}`"
+            @click="toggle(index)"
+          >
+            <template #right-icon>
+              <van-checkbox
+                :name="item"
+                ref="checkboxes"
+                checked-color="#28D190"
+              />
+            </template>
+          </van-cell>
+        </van-cell-group>
+      </van-checkbox-group>
+    </div>
+    <div class="footer">
+      <van-button>发布</van-button>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { Checkbox, CheckboxGroup, Button } from "vant";
+@Component({
+  components: {
+    [Button.name]: Button,
+    [Checkbox.name]: Checkbox,
+    [CheckboxGroup.name]: CheckboxGroup,
+  },
+})
+export default class Home extends Vue {
+  symptonList = [
+    "呕吐、反胃",
+    "腹泻",
+    "感觉乏力、疲惫",
+    "出现皮疹",
+    "发热",
+    "身体疼痛",
+    "体重变化",
+  ];
+  result = [];
+  public toggle(index: number) {
+    // @ts-ignore
+    this.$refs.checkboxes[index].toggle();
+  }
+}
+</script>
+<style lang="less" scoped>
+@import url("./index.less");
+</style>;
